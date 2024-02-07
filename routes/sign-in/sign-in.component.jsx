d729ab9f8af458dd77717b8c69d066f3f24d245a
Arminas Pilotas <Arminas.Pilotas@bdo.global>
2024-02-07 21:19:13 +0200

    Added auth with google redirect

--- routes/sign-in/sign-in.component.jsx
+++ routes/sign-in/sign-in.component.jsx
@@ -1,14 +1,26 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../src/utils/firebase/firebase.utils'
+import { userEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from '../../src/utils/firebase/firebase.utils';
 
 const SignIn = () => {
+    userEffect(async () => {
+        const response = await getRedirectResult(auth);
+        
+        if (response){
+            const userDocRef = await createUserDocumentFromAuth(user);
+        }
+    }, []);
+
     const logGoogleUser = async () => {
         const {user}  = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-    }
+    };
+
     return (
         <div>
             <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>Sign in with google popup</button>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
         </div>
     );
 };
