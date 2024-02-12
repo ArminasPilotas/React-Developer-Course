a6e96271f4e02517e477af273b0287523a10acd3
Arminas Pilotas <Arminas.Pilotas@bdo.global>
2024-02-12 19:31:36 +0200

    created sign-up component

--- routes/sign-in/sign-in.component.jsx
+++ routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import { userEffect } from 'react';
 import { getRedirectResult } from 'firebase/auth';
 import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from '../../src/utils/firebase/firebase.utils';
+import SignUpForm from '../../components/sign-up-form/sign-up.component';
 
 const SignIn = () => {
     userEffect(async () => {
@@ -21,6 +22,7 @@ const SignIn = () => {
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
             <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+            <SignUpForm />
         </div>
     );
 };
