357c1f4e1a6f71cadb46b566e1948785773e3b85
Arminas Pilotas <Arminas.Pilotas@bdo.global>
2024-02-29 18:45:26 +0200

    Added initial product card & cart components

--- routes/navigation/navigation.component.jsx
+++ routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrownLogo } from "../../src/assets/crown.svg";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../src/contexts/user.context";
 import "./navigation.styles.scss";
 import { signOutUser } from "../../src/utils/firebase/firebase.utils";
@@ -27,7 +29,9 @@ const Navigation = () => {
               SIGN-IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
