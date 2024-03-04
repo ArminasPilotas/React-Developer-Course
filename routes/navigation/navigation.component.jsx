7963a1bc092c2000d92a6c805ef9b4ba79e4cc37
Arminas Pilotas <Arminas.Pilotas@bdo.global>
2024-03-04 21:38:27 +0200

    Trying out styling components instead of saas

--- routes/navigation/navigation.component.jsx
+++ routes/navigation/navigation.component.jsx
@@ -5,8 +5,9 @@ import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../src/contexts/user.context";
 import { CartContext } from "../../src/contexts/cart.context";
-import "./navigation.styles.scss";
 import { signOutUser } from "../../src/utils/firebase/firebase.utils";
+
+import { NavigationContainer, NavLinks, NavLink, LogoContainer } from "../navigation/navigation.styles";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -14,27 +15,27 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
+      <NavigationContainer>
+        <LogoContainer to="/">
           <CrownLogo className="logo" />
-        </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">
             SHOP
-          </Link>
+          </NavLink>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>
+            <NavLink as='span' onClick={signOutUser}>
               SIGN OUT
-            </span>
+            </NavLink>
           ) : (
-            <Link className="nav-link" to="/auth">
+            <NavLink to="/auth">
               SIGN-IN
-            </Link>
+            </NavLink>
           )}
           <CartIcon />
-        </div>
+        </NavLinks>
         {isCartOpen && <CartDropdown />}
-      </div>
+      </NavigationContainer>
       <Outlet />
     </Fragment>
   );
